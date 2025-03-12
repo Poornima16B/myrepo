5a35561306ad40d3aaf30eee2456adbfc15634a6
poorn <poorn@DESKTOP-JA5F4QC>
2025-03-12 22:49:05 +0530

    commit 2

--- tnsif/src/tnsif/program.java
+++ tnsif/src/tnsif/program.java
@@ -1,5 +1,21 @@
 package tnsif;
 
+import java.util.Scanner;
+
 public class program {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner (System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+        System.out.println("Welcome");
+	}
 
 }
