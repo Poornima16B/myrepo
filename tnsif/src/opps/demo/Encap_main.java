9d56384f1d516fa37e3ade6f4ca543bdf61a02f3
poorn <poorn@DESKTOP-JA5F4QC>
2025-03-14 13:26:51 +0530

    commit 3

---
+++ tnsif/src/opps/demo/Encap_main.java
@@ -0,0 +1,20 @@
+package opps.demo;
+
+public class Encap_main {
+
+	public static void main(String[] args) {
+
+				
+			
+				EncapDemo obj = new EncapDemo(); 
+				obj.setSerialNum(071);
+				obj.setName("poorni");
+				obj.setAge(21);
+				System.out.println(obj);
+				
+			
+		
+
+	}
+
+}
